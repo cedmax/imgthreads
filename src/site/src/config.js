ee11d148828e1ef4ade07e74ac2cefef134a7547
Marco Cedaro <cedmax@users.noreply.github.com>
2020-05-25 21:03:46 +0100

    removed the concept of staging

--- src/site/src/config.js
+++ src/site/src/config.js
@@ -1,9 +1,4 @@
 const config = {
-  stage: {
-    lambdaURL:
-      'https://75pjlrszfi.execute-api.eu-west-1.amazonaws.com/stage/upload',
-    imgBaseURL: 'https://img-threads-stage.s3-eu-west-1.amazonaws.com/',
-  },
   dev: {
     lambdaURL:
       'https://un314j0ci9.execute-api.eu-west-1.amazonaws.com/dev/upload',
